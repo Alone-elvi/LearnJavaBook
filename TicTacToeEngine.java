cfd1adccab083b60b27a69b6a865da2c7648f9a5
Alone-elvi <alone.elvi@gmail.com>
2021-07-23 00:56:19 +0300

    Проверить правильность работы скрипта. Добавить строку для отображения хода игры

--- TicTacToeEngine.java
+++ TicTacToeEngine.java
@@ -53,48 +53,162 @@ public class TicTacToeEngine implements ActionListener {
         }
     }
 
+    // делаем недоступными клетки и доступной кнопку "NewGame"
     private void endTheGame() {
+        parent.button_new_game.setEnabled(true);
+        for (int i = 0; i < 9; i++) {
+            parent.buttons[i].setEnabled(false);
+        }
     }
 
     /*
-    * Этот метод прменяет набор правил, чтобы найти лучший компьютерный ход. Если хороший ход не найден, выбирается
-    * случайная клетка.
-    * */
+     * Этот метод прменяет набор правил, чтобы найти лучший компьютерный ход. Если хороший ход не найден, выбирается
+     * случайная клетка.
+     * */
 
     private void computerMove() {
         int selectedSquare;
 
-    // Сначала компьютер пытается найти пустую клетку рядом с двумя клетками, чтобы выиграть.
+        // Сначала компьютер пытается найти пустую клетку рядом с двумя клетками, чтобы выиграть.
 
         selectedSquare = findEmptySquare("O");
-        
-    // Если он не может найти 2 нолика, то хотя бы не даёт оппоненту сделать ряд из 3-х крестиков, поместив нолик рядом
-    // с 2-мя крестиками.
 
-        if (selectedSquare == -1){
+        // Если он не может найти 2 нолика, то хотя бы не даёт оппоненту сделать ряд из 3-х крестиков, поместив нолик рядом
+        // с 2-мя крестиками.
+
+        if (selectedSquare == -1) {
             selectedSquare = findEmptySquare("X");
         }
 
-    // Если selectedSquare ысё ещё равен -1, то пытается занять центральную клетку.
-        if (selectedSquare == -1 &&  (parent.buttons[4].getLabel().equals("")) ){
+        // Если selectedSquare ысё ещё равен -1, то пытается занять центральную клетку.
+        if (selectedSquare == -1 && (parent.buttons[4].getLabel().equals(""))) {
             selectedSquare = 4;
         }
 
-    // Не повезло с центральной  просто занимаем любую случайную клетку
-        if (selectedSquare == -1){
+        // Не повезло с центральной  просто занимаем любую случайную клетку
+        if (selectedSquare == -1) {
             selectedSquare = getRandomSquare();
         }
         parent.buttons[selectedSquare].setLabel("0");
     }
 
-//    @TODO дописать метод getRandomSquare
+    //  Этот метод выбирает любую пустую клетку
+//  @return случайно выборанный номер клетки
     private int getRandomSquare() {
-        return 1;
+        boolean gotEmptySquare = false;
+        int selectedSquare = -1;
+
+        do {
+            selectedSquare = (int) (Math.random() * 9);
+            if (parent.buttons[selectedSquare].getLabel().equals("")) {
+                gotEmptySquare = true; // Чтобы закончить цикл
+            }
+        } while (!gotEmptySquare);
+        return selectedSquare;
     }
 
+    /*
+     * Этот метод проверяет каждый ряд, колонку и диагональ, чтобы узнать, есть ли в ней 2 клетки с одинаковыми надписями
+     * и пустой клеткой
+     * @param передаётся "Х" для пользователя и "О" для компа
+     * @return количество свободных клеток, или -1, если не найдено 2 клетки с одинаковыми надписями
+     * */
 
-    private int findEmptySquare(String o) {
-        return 1;
+    private int findEmptySquare(String player) {
+        int weight[] = new int[9];
+        for (int i = 0; i < 9; i++) {
+            if (parent.buttons[i].getLabel().equals("O")) {
+                weight[i] = -1;
+            } else if (parent.buttons[i].getLabel().equals("X")) {
+                weight[i] = 1;
+            } else {
+                weight[i] = 0;
+            }
+        }
+        int twoWeights = player.equals("O") ? -2 : 2;
+
+        // Проверим есть ли в ряду 1 2 однинаковые кллетки и  одна пустая?
+        if (weight[0] + weight[1] + weight[2] == twoWeights) {
+            if (weight[0] == 0) {
+                return 0;
+            } else if (weight[1] == 0) {
+                return 1;
+            } else {
+                return 2;
+            }
+        }
+        // Проверим есть ли в ряду 2 2 однинаковые кллетки и  одна пустая?
+        if (weight[3] + weight[4] + weight[5] == twoWeights) {
+            if (weight[3] == 0) {
+                return 3;
+            } else if (weight[4] == 0) {
+                return 4;
+            } else {
+                return 5;
+            }
+        }
+        // Проверим есть ли в ряду 3 2 однинаковые кллетки и  одна пустая?
+        if (weight[6] + weight[7] + weight[8] == twoWeights) {
+            if (weight[6] == 0) {
+                return 6;
+            } else if (weight[7] == 0) {
+                return 7;
+            } else {
+                return 8;
+            }
+        }
+        // Проверим есть ли в колонке 1 2 однинаковые кллетки и  одна пустая?
+        if (weight[0] + weight[3] + weight[6] == twoWeights) {
+            if (weight[0] == 0) {
+                return 0;
+            } else if (weight[3] == 0) {
+                return 3;
+            } else {
+                return 6;
+            }
+        }
+        // Проверим есть ли в колонке 2 2 однинаковые кллетки и  одна пустая?
+        if (weight[1] + weight[4] + weight[7] == twoWeights) {
+            if (weight[1] == 0) {
+                return 1;
+            } else if (weight[4] == 0) {
+                return 4;
+            } else {
+                return 7;
+            }
+        }
+        // Проверим есть ли в колонке 3 2 однинаковые кллетки и  одна пустая?
+        if (weight[2] + weight[5] + weight[8] == twoWeights) {
+            if (weight[2] == 0) {
+                return 2;
+            } else if (weight[5] == 0) {
+                return 5;
+            } else {
+                return 8;
+            }
+        }
+        // Проверим есть ли в диагонали 1 2 однинаковые кллетки и  одна пустая?
+        if (weight[0] + weight[4] + weight[8] == twoWeights) {
+            if (weight[0] == 0) {
+                return 0;
+            } else if (weight[4] == 0) {
+                return 4;
+            } else {
+                return 8;
+            }
+        }
+        // Проверим есть ли в диагонали 2 2 однинаковые кллетки и  одна пустая?
+        if (weight[2] + weight[4] + weight[6] == twoWeights) {
+            if (weight[2] == 0) {
+                return 2;
+            } else if (weight[4] == 0) {
+                return 4;
+            } else {
+                return 6;
+            }
+        }
+        // Не найдено двух одинаковых соседних клеток
+        return -1;
     }
 
 
@@ -172,6 +286,9 @@ public class TicTacToeEngine implements ActionListener {
         return theWinner;
     }
 
-    private void highlightWinner(int i, int i1, int i2) {
+    private void highlightWinner(int win1, int win2, int win3) {
+        parent.buttons[win1].setBackground(Color.CYAN);
+        parent.buttons[win2].setBackground(Color.CYAN);
+        parent.buttons[win3].setBackground(Color.CYAN);
     }
 }
